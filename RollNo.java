33d7307e21e2e37c266faecd34e55eafdb2681e5
Shwetahiremath57 <shwetahiremath17xworkz@gmail.com>
2021-12-20 17:27:35 +0530

    added : practice java file

---
+++ RollNo.java
@@ -0,0 +1,10 @@
+class RollNo{
+                             public static void main( String []arg){
+                             String city="hubli";
+                             System.out.println(city);
+                            
+                            
+               }
+}                           
+                             
+                             
